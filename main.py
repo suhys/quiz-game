281377800a87707b237c1a12e4457f3cef81bcb3
yuseung suh <yuseung.suh@gmail.com>
2026-08-12 18:09:35 +0900

    Feat: implement menu functionality

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 class QuizGame:
+    """ Manages the overall game flows and data """
+
+    # create quizzes list and initial setting
     def __init__(self):
         self.quizzes = []
         self.best_scsore = 0
+        self.max_menu_option = 5
+        self.min_menu_option = 1
 
-    def menu(self):
+    @staticmethod
+    # user input
+    def input(user_menu_input, min_value, max_value):
+        """
+        Get an integer input within min and max value
+        - Strips whitespace, catches conversion failures, out-of-range, and empty input.
+        - Returns None on any problem so the user can re-prompt
+        """
+        user_input = input(user_menu_input).strip()
+
+        if user_input =="":
+            print(f"⚠️ 입력이 비어있습니다. {min_value}-{max_value} 사이의 숫자를 입력하세요.")
+            return None
+
+        try:
+            value = int(user_input)
+        except ValueError:
+            print(f"⚠️ 잘못된 입력입니다. {min_value}-{max_value} 사이의 숫자를 입력하세요.")
+            return None
+
+        if not (min_value <= value <= max_value):
+            print(f"⚠️ 잘못된 입력입니다. {min_value}-{max_value} 사이의 숫자를 입력하세요.")
+            return None
+
+        return value
+
+    def print_menu(self):
          print("=" * 40)
          print("quiz game")
          print("1. 퀴즈풀기")
@@ -14,12 +45,14 @@ class QuizGame:
          print("=" * 40)
 
     def run(self):
+        """ Main menu loop. Handles exit safely"""
         try:
             while True:
-                self.show_menu()
-                choice = self.get_int_input("선택: ")
+                self.print_menu()
+                choice = self.get_int_input("선택: ", self.min_menu,self.max_menu_option)
                 if choice is None:
                     continue
+
                 if choice == 1:
                     print("")
                 elif choice == 2:
@@ -35,9 +68,9 @@ class QuizGame:
             print("\n\n⚠️ 입력이 중단되었습니다. 안전하게 종료합니다.")
 
 
-
-
-
 def main():
         game = QuizGame()
         game.run()
+
+if __name__ == "__main__":
+    main()
